d5e3fa6f184df55d5893c1a0204f4a243c266915
y2ahhh <166089431+y2ahhh@users.noreply.github.com>
2026-06-04 10:41:42 +0900

    add: yeaheun-12910
    * Add: yeaheun-12910
    
    * Add: yeaheun-12910
    
    * Add: yeaheun-12912
    
    * Add: yeaheun-87389
    
    * Add: yeaheun-12916
    
    * Add: yeaheun-76501
    
    * Add: yeaheun-86051
    
    * Add: yeaheun-12910

--- programmers/week3/12910/이예은.java
+++ programmers/week3/12910/이예은.java
@@ -1,9 +1,15 @@
 class Solution {
-    public int solution(int n) {
-        int answer=0; // 이거 없어도 되는데 푸시 실패해서 걍 넣엇음요
-        for(int i=1; i<n; i++){
-            if(n%i==1) return i;
+    public int[] solution(int[] arr, int divisor) {
+        int[] array = Arrays.stream(arr)
+                .filter(a -> a % divisor == 0)
+                .sorted()
+                .toArray();
+
+        if(array.length==0) {
+            int[] i = {-1};
+            return i;
         }
-        return 0;
-    }
+        else return array;
+  }
 }
+  
