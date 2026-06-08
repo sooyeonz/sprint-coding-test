210babdfc157c738b998deb5fa74624c9c899430
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-06-08 10:30:34 +0900

    Add: jiwon-12943 (#83)

---
+++ programmers/week3/12943/함지원.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+
+        if (num == 1) return 0;
+
+        while (num != 1) {
+
+            if (answer == 400) return -1;
+
+            if (num % 2 == 0) num /= 2;
+            else num = (num * 3) + 1;
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
