595eb2b1d7a0304d8a173d981b6702877691c22c
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-06-08 10:30:19 +0900

    Add: jiwon-12919 (#82)

---
+++ programmers/week3/12919/함지원.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] seoul) {
+
+        int i = Arrays.asList(seoul).indexOf("Kim");
+
+        return "김서방은 " + i + "에 있다";
+    }
+}
