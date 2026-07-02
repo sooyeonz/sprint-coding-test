182be5f3917305377c862d5aadc0c5689fdd83a1
sooyeonz <5196ksy@naver.com>
2026-07-02 22:38:45 +0900

    Add: sooyeon-12953

---
+++ programmers/week7/day3/12953/김수연.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public int solution(int[] arr) {
+        int result = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            result = lcm(result, arr[i]);
+        }
+        return result;
+    }
+}
